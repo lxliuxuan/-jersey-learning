5a3922f701816065b1c1d0cfc5d43f5c75d15c84
刘玄 <389351782@qq.com>
2016-11-11 16:03:27 +0800

    使用Main启动程序

---
+++ chapter1-3/src/main/java/cn/lx/application/Application.java
@@ -0,0 +1,17 @@
+package cn.lx.application;
+
+import org.glassfish.jersey.server.ResourceConfig;
+
+import javax.ws.rs.ApplicationPath;
+
+/**
+ * Created by lxliuxuan on 2016/11/11.
+ */
+@ApplicationPath("/")
+public class Application extends ResourceConfig{
+    public Application() {
+        /*声明资源包所在位置
+        * */
+        packages("cn.lx.resource");
+    }
+}
